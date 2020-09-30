d22f9ebe0e8bda707fa5b7d224d4083a54e2cb4c
Matt Pritchett <matthew.r.pritchett@gmail.com>
2020-09-30 16:28:06 -0400

    Update EventServiceProvider.php

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MRP\NotificationSubscriptions\Providers;
+namespace Mrpritchett\NotificationSubscriptions\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -8,7 +8,7 @@ class EventServiceProvider extends ServiceProvider
 {
 	protected $listen = [
 	    'Illuminate\Notifications\Events\NotificationSending' => [
-	        'MRP\NotificationSubscriptions\Listeners\NotificationSendingListener',
+	        'Mrpritchett\NotificationSubscriptions\Listeners\NotificationSendingListener',
 	    ],
     ];
 
